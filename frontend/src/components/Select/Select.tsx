a3efbb212ecbcecb8c8c9d0c3d47b46ff04841dc
wonderize <lowlenexbot002@gmail.com>
2023-08-09 16:51:34 +0300

    `Select` component modified

--- frontend/src/components/Select/Select.tsx
+++ frontend/src/components/Select/Select.tsx
@@ -3,10 +3,20 @@ import styles from "./Select.module.css"
 
 export default function Select() {
   return (
-    <div className={`${inputStyle.input} ${styles.wrapper}`}>
-      <select className={styles.select}>
-        <option value="" className={styles.option}>
+    <div className={`${inputStyle.input} ${styles.wrapper}`} aria-required>
+      <select className={styles.select} name="name" required>
+        <option
+          value=""
+          disabled
+          hidden
+          selected
+        >
           Default option
+        </option>
+        <option
+          value="value"
+        >
+          Value
         </option>
       </select>
       <span className={styles.focus} />
