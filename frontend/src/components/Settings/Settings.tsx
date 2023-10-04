f79137382b2c66727674bf02e148de795deae36a
Luerl21 <dmyt20042@gmail.com>
2023-10-04 13:13:42 +0300

    Component Settings

--- frontend/src/components/Settings/Settings.tsx
+++ frontend/src/components/Settings/Settings.tsx
@@ -2,7 +2,44 @@ import Select from "@/components/Select/Select"
 
 import styles from "./Settings.module.css"
 
+<<<<<<< Updated upstream
 export default function Settings() {
+=======
+// FIXME: Fix this interface
+export interface SettingsProps {
+  selectStates: [any, Dispatch<SetStateAction<any>>][]
+}
+
+export default function Settings({ selectStates }: SettingsProps) {
+  const [theme, setTheme] = selectStates[0]
+  const [group, setGroup] = selectStates[1]
+  // FIXME: Fix this types
+  const [groups, setGroups]: [any, any] = useState([])
+
+  // FIXME: Fix this types
+  const themes: any = [
+    {
+      value: "system",
+      title: "Системная",
+    },
+    {
+      value: "dark",
+      title: "Темная",
+    },
+    {
+      value: "light",
+      title: "Cветлая",
+    },
+  ]
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/groups")
+      .then((data) => data.json())
+      .then((data) => setGroups(data))
+      .catch((error) => console.error(error))
+  }, [])
+
+>>>>>>> Stashed changes
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Настройки</h1>
