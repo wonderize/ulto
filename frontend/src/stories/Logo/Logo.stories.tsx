16e86433703da215775f20ee60e42bf1336d6882
Luerl21 <dmyt20042@gmail.com>
2023-10-04 13:14:24 +0300

    Story Logo

---
+++ frontend/src/stories/Logo/Logo.stories.tsx
@@ -0,0 +1,12 @@
+import Logo from '../../components/Logo/Logo';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Logo> = {
+  component: Logo,
+};
+
+export default meta;
+type Story = StoryObj<typeof Logo>;
+
+export const Primary: Story = {
+};
